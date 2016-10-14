d82e584e70f37b1b24f15008a72c72e7095b28ce
anwarshahriar <anwarshahriar@gmail.com>
2016-10-14 23:37:11 +0600

    Write more reusable react components with composeable APIs

--- App.js
+++ App.js
@@ -4,58 +4,70 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor() {
         super();
-        this.state = { val: 0 };
+        this.state = { 
+            red: 0
+        };
         this.update = this.update.bind(this);
     }
 
     update() {
-        this.setState({ val: this.state.val + 1 })
-    }
-
-    componentWillMount() {
-        this.setState({ m: 2 })
-    }
-
-    render() {
-        console.log('rendering!')
-        return (
-            <button onClick={this.update}>
-                {this.state.val * this.state.m}
-            </button>
-        )
-    }
-
-    componentDidMount() {
-        this.inc = setInterval(this.update, 1000)
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.inc)
-    }
-}
-
-class Wrapper extends React.Component {
-    constructor() {
-        super();
-    }
-
-    mount() {
-        ReactDOM.render(<App />, document.getElementById('a'))
-    }
-
-    unmount() {
-        ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+        this.setState({ 
+            red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value 
+        })
     }
 
     render() {
         return (
             <div>
-                <button onClick={this.mount.bind(this)}>Mount</button>
-                <button onClick={this.unmount.bind(this)}>Unmount</button>
-                <div id="a"></div>
+                <NumInput ref="red" 
+                    min={0}
+                    max={255}
+                    step={0.01}
+                    val={+this.state.red}
+                    label='Red'
+                    type='range'
+                    update={this.update} />
             </div>
         )
     }
 }
 
-export default Wrapper
+class NumInput extends React.Component {
+    render() {
+        let label = this.props.label !== '' ? 
+            <label>{this.props.label} - {this.props.val}</label> : ''
+        return (
+            <div>
+                <input ref="inp" 
+                    type={this.props.type}
+                    min={this.props.min}
+                    max={this.props.max}
+                    step={this.props.step}
+                    defaultValue={this.props.val}
+                    onChange={this.props.update} />
+                    {label}
+            </div>
+        )
+    }
+}
+
+NumInput.propTypes = {
+    min: React.PropTypes.number,
+    max: React.PropTypes.number,
+    step: React.PropTypes.number,
+    val: React.PropTypes.number,
+    label: React.PropTypes.string,
+    update: React.PropTypes.func.isRequired,
+    type: React.PropTypes.oneOf(['number', 'range'])
+}
+
+NumInput.defaultProps = {
+    min: 0,
+    max: 0,
+    step: 1,
+    val: 0,
+    label: '',
+    type: 'range'
+}
+
+export default App
